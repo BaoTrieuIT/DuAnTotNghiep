f257f180dc9d8fcb5a9c4a4b7169051aa30ee2bd
TrieuBao <baotrieu27122003@gmail.com>
2023-08-27 19:08:49 +0700

    Create prj

---
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/WebsiteNoRulesShopApplication.java
@@ -0,0 +1,13 @@
+package com.poly.website_norulesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteNoRulesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteNoRulesShopApplication.class, args);
+	}
+
+}
