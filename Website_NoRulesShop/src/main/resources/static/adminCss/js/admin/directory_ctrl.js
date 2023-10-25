18b9b2af8d54b85e7e1cea50390c753c08eb48a8
phongcuncon <96615152+phongcuncon@users.noreply.github.com>
2023-10-26 05:06:19 +0700

    thêm được danh mục con nhưng vẫn còn lỗi

--- Website_NoRulesShop/src/main/resources/static/adminCss/js/admin/directory_ctrl.js
+++ Website_NoRulesShop/src/main/resources/static/adminCss/js/admin/directory_ctrl.js
@@ -1,4 +1,17 @@
-app.controller("directory_ctrl", function($scope, $http){
+app.service('DataSharingService', function() {
+    var directoryId = null;
+
+    return {
+        getDirectoryId: function() {
+            return directoryId;
+        },
+        setDirectoryId: function(newDirectoryId) {
+            directoryId = newDirectoryId;
+        }
+    };
+});
+
+app.controller("directory_ctrl", function($scope, $http ,DataSharingService){
     $scope.initialize = function(){
         $http.get("/rest/manage_gender").then(resp => {
             $scope.genders = resp.data
@@ -7,7 +20,6 @@ app.controller("directory_ctrl", function($scope, $http){
             $scope.items = resp.data;
         })
     }
-
 
     $scope.genderSelected = null;
 
@@ -18,16 +30,15 @@ app.controller("directory_ctrl", function($scope, $http){
         })
     }
 
-
-
-    $scope.ClickDirectory = function (DirectoryId){
-        $http.get("../rest/manage_directory/Directory?directoryId="+ DirectoryId).then(resp => {
+    $scope.ClickDirectory = function (directoryId){
+        $http.get("/rest/manage_directory/Directory?directoryId="+ directoryId).then(resp => {
             $scope.ListdirectoryLv1 = resp.data
+            DataSharingService.setDirectoryId(directoryId)
         })
     }
 
     $scope.createDir = function(genderID){
-        $scope.form.genderId = genderID
+        // $scope.form.genderId = genderID
         var item = angular.copy($scope.form);
         console.log(item)
         $http.post("/rest/manage_directory/"+genderID ,item).then(resp => {
@@ -40,19 +51,47 @@ app.controller("directory_ctrl", function($scope, $http){
         });
     }
 
+    $scope.initialize();
 
-    // $scope.createDirLv1 = function(DirectoryId){
-    //     var item = angular.copy($scope.form);
-    //     $http.post('../rest/manage_directory/Directory?directoryId='+ DirectoryId + item).then(resp => {
-    //         $scope.directoryList.push(resp.data);
-    //         $scope.form= {};
-    //         alert("Thêm mới thành công!");
-    //     }).catch(error => {
-    //         alert("Lỗi thêm mới!");
-    //         console.log("Error", error);
-    //     });
-    // }
-    //
+})
+
+
+
+app.controller("directoryLv1_ctrl", function($scope, $http, DataSharingService){
+    var direcId = null
+    // $scope.directoryId = DataSharingService.getDirectoryId()
+    $scope.$watch(function (){
+        return DataSharingService.getDirectoryId()
+    }, function(newDirectoryId, oldDirectoryId){
+        if (newDirectoryId !== oldDirectoryId) {
+            console.log(newDirectoryId)
+            // Gọi lại API khi directoryId thay đổi
+            direcId = newDirectoryId
+        }
+    });
+
+
+    $scope.initialize = function(direcId){
+        $http.get("/rest/manage_directory/Directory?directoryId="+ direcId).then(resp => {
+            $scope.ListdirectoryLv1 = resp.data;
+        })
+    }
+
+    $scope.creatDirLv1 = function(direcId){
+        console.log($scope.ListdirectoryLv1)
+        console.log(direcId)
+        console.log($scope.directoryLv1Name)
+        var dlv1 = angular.copy($scope.form)
+        $http.post('/rest/directoryLv1/'+ direcId,dlv1).then(resp => {
+            $scope.ListdirectoryLv1.push(resp.data);
+            $scope.form= {};
+            alert("Thêm mới thành công!");
+        }).catch(error => {
+            alert("Lỗi thêm mới!");
+            console.log("Error", error);
+        });
+    }
+
     // $scope.update = function(item){
     //     var item = angular.copy($scope.form);
     //     $http.put(`/rest/directoryLv1/${item.id}`, item).then(resp => {
@@ -65,7 +104,7 @@ app.controller("directory_ctrl", function($scope, $http){
     //             console.log("Error", error);
     //         });
     // }
-    //
+
     // $scope.delete = function(item){
     //     if(confirm("Bạn muốn xóa sản phẩm này?")){
     //         $http.delete(`/rest/directoryLv1/${item.id}`).then(resp => {
@@ -78,6 +117,23 @@ app.controller("directory_ctrl", function($scope, $http){
     //         })
     //     }
     // }
+    // $scope.deleteSelectedItems = function() {
+    //     var selectedItems = $scope.ListdirectoryLv1.filter(function(item) {
+    //         return item.checked;
+    //     });
+    //
+    //     // Biến đổi danh sách các mục đã đánh dấu thành một mảng của các ID hoặc đối tượng Directory
+    //     var selectedIds = selectedItems.map(function(item) {
+    //         return item.directoryId;
+    //     });
+    //
+    //     $http.delete('/api/directories/delete', { data: selectedIds }).then(function(response) {
+    //         // Xử lý phản hồi từ máy chủ (nếu cần)
+    //     });
+    // };
+
     $scope.initialize();
 })
+
+
 
