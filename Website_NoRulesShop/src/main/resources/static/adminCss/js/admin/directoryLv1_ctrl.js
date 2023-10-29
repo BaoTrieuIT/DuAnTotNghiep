4855e10784aac41b70af7bce300cc9e38b467296
phongcuncon <96615152+phongcuncon@users.noreply.github.com>
2023-10-29 15:46:21 +0700

    xóa ngu

--- Website_NoRulesShop/src/main/resources/static/adminCss/js/admin/directoryLv1_ctrl.js
+++ Website_NoRulesShop/src/main/resources/static/adminCss/js/admin/directoryLv1_ctrl.js
@@ -21,13 +21,10 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
     //         })
     //     }
     //
-    $scope.initialize = function (direcId) {
-        $http.get("/rest/manage_directory/Directory?directoryId=" + direcId).then(resp => {
+    $scope.initialize = function(direcId){
+        $http.get("/rest/manage_directory/Directory?directoryId="+ direcId).then(resp => {
             $scope.ListdirectoryLv1 = resp.data;
         })
-    }
-    $scope.select = function (direcId) {
-        console.log(direcId)
     }
     // Ban đầu ẩn form
     $scope.isCreateDirectoryLv1ModalOpen = false;
@@ -37,7 +34,7 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
         $scope.isCreateDirectoryLv1ModalOpen = true;
     }
 
-
+// THÊM MỚI DIRECTORY LEVEL 1
     $scope.creatDirLv1 = function (direcId) {
         console.log($scope.ListdirectoryLv1)
         console.log(direcId)
@@ -45,15 +42,16 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
         var dlv1 = angular.copy($scope.form)
         $http.post('/rest/directoryLv1/' + direcId, dlv1).then(resp => {
             $scope.ListdirectoryLv1.push(resp.data);
-            $scope.form = {};
             alert("Thêm mới thành công!");
+            $scope.form = {};
+            $scope.isCreateDirectoryLv1ModalOpen = false;
         }).catch(error => {
             alert("Lỗi thêm mới!");
             console.log("Error", error);
         });
     }
 
-    // CẬP NHẬT DIRECTORY LEVEL 1
+// CẬP NHẬT DIRECTORY LEVEL 1
     $scope.selectedDirectoryLv1 = null
 
     $scope.selectDirectoryLv1 = function (directoryLv1){
@@ -78,6 +76,7 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
             $scope.ListdirectoryLv1[index] = item;
             // $scope.showForm = false
             alert("Cập nhật sản phẩm thành công!");
+            $('#edit_name_directory_lv1_' + item.directoryLv1Id).modal('hide')
             $scope.selectedDirectoryLv1 = {};
         }).catch(error => {
             alert("Lỗi cập nhật sản phẩm!");
@@ -85,23 +84,6 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
         });
     };
 
-
-    $scope.deleteSelectedItems = function() {
-        var selectedItems = $scope.ListdirectoryLv1.filter(function(directoryLv1) {
-            return directoryLv1.checked;
-            console.log(directoryLv1.directoryLv1Name)
-        });
-
-        // Biến đổi danh sách các mục đã đánh dấu thành một mảng của các ID hoặc đối tượng Directory
-        var selectedIds = selectedItems.map(function(directoryLv1) {
-            return directoryLv1.directoryLv1Id;
-            console.log(directoryLv1.directoryLv1Name)
-        });
-
-        $http.delete('/rest/directoryLv1/delete', { data: selectedIds }).then(function(response) {
-            // Xử lý phản hồi từ máy chủ (nếu cần)
-        });
-    };
     // $scope.delete = function(item){
     //     if(confirm("Bạn muốn xóa sản phẩm này?")){
     //         $http.delete(`/rest/directoryLv1/${item.id}`).then(resp => {
@@ -114,5 +96,38 @@ app.controller("directoryLv1_ctrl", function ($scope, $http, DataSharingService)
     //         })
     //     }
     // }
+
+    $scope.deleteSelectedItems = function() {
+        var selectedIds = [];
+
+        // Lặp qua danh sách các mục đã đánh dấu
+        for (var i = 0; i < $scope.ListdirectoryLv1.length; i++) {
+            if ($scope.ListdirectoryLv1[i].checked) {
+                selectedIds.push($scope.ListdirectoryLv1[i].directoryLv1Id);
+                console.log($scope.ListdirectoryLv1[i].directoryLv1Name);
+            }
+        }
+        console.log(selectedIds)
+        if (selectedIds.length === 0) {
+            alert("Vui lòng chọn ít nhất một mục để xóa.");
+            return;
+        }
+
+        // Gửi các ID đã chọn lên máy chủ để xóa
+        $http({
+            method: 'DELETE',
+            url: '/rest/directoryLv1/delete',
+            data: selectedIds, // Gửi mảng các ID cần xóa
+            headers: { 'Content-Type': 'application/json;charset=utf-8' }
+        }).then(function(response) {
+            // Xử lý phản hồi từ máy chủ (nếu cần)
+            alert("Xóa thành công")
+        }).catch(function(error) {
+            alert("Lỗi xóa mục đã chọn.");
+            console.error("Error", error);
+        });
+    };
+
+
     $scope.initialize();
 })
