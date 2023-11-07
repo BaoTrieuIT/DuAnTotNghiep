8212a3be40443b4a2acb90cddaf094d45002fba5
TrieuBao <baotrieu27122003@gmail.com>
2023-11-07 15:13:52 +0700

    fix-product

--- Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/service/ProductService.java
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/service/ProductService.java
@@ -1,8 +1,30 @@
 package com.poly.website_norulesshop.service;
 
+import com.poly.website_norulesshop.entity.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ProductService {
-  
+    void saveProduct(Product product);
+
+    Product getProductById(Integer id);
+
+    List<Product> getAllProducts();
+
+    void deleteProduct(Integer id);
+
+    List<Product> SearchProductActiveByName(String productName);
+
+    List<Product> SearchProductHiddenByName(String productName);
+
+    List<Product> SearchProductSoldoutByName(String productName);
+
+
+    List<Product> isHidden();
+
+    List<Product> isActive();
+
+    List<Product> isSoldOut();
 }
