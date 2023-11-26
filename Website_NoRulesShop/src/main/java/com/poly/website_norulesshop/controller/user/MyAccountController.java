683a67a7da6747c89c34e1fa2052801bdc2915d6
TrieuBao <baotrieu27122003@gmail.com>
2023-11-26 21:53:37 +0700

    fix_image

--- Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/user/MyAccountController.java
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/user/MyAccountController.java
@@ -2,6 +2,7 @@ package com.poly.website_norulesshop.controller.user;
 
 import com.poly.website_norulesshop.entity.Account;
 import com.poly.website_norulesshop.service.AccountService;
+import com.poly.website_norulesshop.utils.FileUploadUtil;
 import com.poly.website_norulesshop.utils.SessionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,7 @@ public class MyAccountController {
     SessionService session;
     @Autowired
     AccountService accountService;
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static/user/img/avatar/";
 
     @GetMapping("/my-account")
     public String index(Model model) throws InterruptedException {
@@ -50,7 +52,7 @@ public class MyAccountController {
                          @RequestParam("password") String newPassword,
                          @RequestParam("comfirmPassword") String comfirmPassword,
                          @RequestParam(value = "changePassword", required = false, defaultValue = "false") boolean changePassword,
-                         Principal principal) {
+                         Principal principal) throws IOException {
         // Lấy thông tin tài khoản từ username của người đăng nhập
         String username = principal.getName();
         Account existingAccount = accountService.findByUsername(username);
@@ -82,126 +84,52 @@ public class MyAccountController {
             }
             acc.setPassword(newPassword);
         }
-
-        // Xử lý ảnh
         try {
-            String directoryPath = "src/main/resources/static/user/img/avatar/";
-
-            // Lấy thông tin từ tên file gốc
             String originalFileName = file.getOriginalFilename();
-            String[] fileNameParts = originalFileName.split("\\.");
-
-// Lấy tên file và đuôi file từ mảng fileNameParts
-            String fileName = fileNameParts[0]; // Tên file (trước dấu chấm)
-            String fileExtension = "png"; // Đặt đuôi file mới
-
-// Tạo tên file mới
-            String newFileName = "avatar_" + acc.getAccount_id() + "_" + fileName + "." + fileExtension;
-
-// Lưu ảnh và đường dẫn vào cơ sở dữ liệu
-            if (!originalFileName.toLowerCase().endsWith(".png")) {
-                BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                ImageIO.write(bufferedImage, fileExtension, byteArrayOutputStream);
-                Files.write(Paths.get(directoryPath, newFileName), byteArrayOutputStream.toByteArray());
-            } else {
-                Files.write(Paths.get(directoryPath, newFileName), file.getBytes());
-            }
-
-// Lưu đường dẫn ảnh vào thuộc tính avatar_url
-            String filename = StringUtils.cleanPath(newFileName);
-            acc.setAvatar_url(filename);
-        } catch (IOException e) {
+            FileUploadUtil.saveFile(UPLOAD_DIRECTORY, originalFileName, file);
+            acc.setAvatar_url(originalFileName);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+
+//        // Xử lý ảnh
+//        try {
+//            String directoryPath = "src/main/resources/static/user/img/avatar/";
+//
+//            // Lấy thông tin từ tên file gốc
+//            String originalFileName = file.getOriginalFilename();
+//            String[] fileNameParts = originalFileName.split("\\.");
+//
+//// Lấy tên file và đuôi file từ mảng fileNameParts
+//            String fileName = fileNameParts[0]; // Tên file (trước dấu chấm)
+//            String fileExtension = "png"; // Đặt đuôi file mới
+//
+//// Tạo tên file mới
+//            String newFileName = "avatar_" + acc.getAccount_id() + "_" + fileName + "." + fileExtension;
+//
+//// Lưu ảnh và đường dẫn vào cơ sở dữ liệu
+//            if (!originalFileName.toLowerCase().endsWith(".png")) {
+//                BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+//                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//                ImageIO.write(bufferedImage, fileExtension, byteArrayOutputStream);
+//                Files.write(Paths.get(directoryPath, newFileName), byteArrayOutputStream.toByteArray());
+//            } else {
+//                Files.write(Paths.get(directoryPath, newFileName), file.getBytes());
+//            }
+//
+//// Lưu đường dẫn ảnh vào thuộc tính avatar_url
+//            String filename = StringUtils.cleanPath(newFileName);
+//            acc.setAvatar_url(filename);
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
 
 
         // Cập nhật thông tin tài khoản
         accountService.updateAccount_frUser(acc);
         model.addAttribute("acc", acc);
         session.set("acc", acc);
-        System.out.println(acc);
-        System.out.println("ID: " + acc.getAccount_id());
         return "redirect:/home/my-account";
     }
 }
-//    @GetMapping("/my-account")
-//    public String index(Model model) throws InterruptedException {
-//        model.addAttribute("title", "Tài khoản của tôi");
-//        Account account = session.get("acc");
-//        String path = "/user/img/avatar/" + account.getAvatar_url();
-//        model.addAttribute("imagePath", path);
-//        model.addAttribute("acc", account);
-//        String generalAddress = account.getAddress().getGeneralAddress();
-//        System.out.println("D/c: "+generalAddress);
-//        String[] addressParts = generalAddress.split(","); // Tách chuỗi dựa trên dấu phẩy
-//
-//        System.out.println("city: "+addressParts[2].trim());
-//        System.out.println("district: "+addressParts[1].trim());
-//        System.out.println("ward: "+ addressParts[0].trim());
-//        // Truyền giá trị cho từng select box
-//        model.addAttribute("city", addressParts[2].trim());
-//        model.addAttribute("district", addressParts[1].trim());
-//        model.addAttribute("ward", addressParts[0].trim());
-//        return "user/my_account";
-//    }
-//    @PostMapping("/my-account/update")
-//    public String update(@RequestParam("image") MultipartFile file,
-//                         @Valid @ModelAttribute Account acc, Model model,
-//                         BindingResult result,
-//                         @RequestParam("password") String newPassword,
-//                         @RequestParam("comfirmPassword") String comfirmPassword,
-//                         @RequestParam(value = "changePassword", required = false, defaultValue = "false") boolean changePassword,
-//                         Principal principal) {
-//        System.out.println("newPassword: " + newPassword);
-//        System.out.println("comfirmPassword: " + comfirmPassword);
-//
-//        // Kiểm tra họ tên không để trống và không có kí tự đặc biệt hoặc số
-//        if (acc.getFullname() == null || !acc.getFullname().matches("^[a-zA-Z\\s]+$")) {
-//            result.rejectValue("fullname", "error.account", "Họ và tên không hợp lệ");
-//            return "user/my_account";
-//        }
-//        // Kiểm tra ngày sinh
-//        if (acc.getBirthday() != null) {
-//            LocalDate today = LocalDate.now();
-//            LocalDate birthday = acc.getBirthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//            long age = ChronoUnit.YEARS.between(birthday, today);
-//
-//            if (age < 12 || age > 200) {
-//                result.rejectValue("birthday", "error.account", "Ngày sinh không đủ 12 tuổi hoặc lớn hơn 200 tuổi");
-//                return "user/my_account";
-//            }
-//        }
-//
-//        // Kiểm tra mật khẩu nếu có sự thay đổi
-//        if (comfirmPassword != null && changePassword && newPassword != null && newPassword.length() > 6) {
-//            if (!newPassword.equals(comfirmPassword)) {
-//                model.addAttribute("errorDiv", "Mật khẩu mới và xác nhận mật khẩu không khớp.");
-//                return "user/my_account";
-//            }
-//            acc.setPassword(newPassword);
-//        }
-//
-//        // Xử lý ảnh
-//        try {
-//            String directoryPath = "src/main/resources/static/user/img/avatar/";
-//            Path fileNameAndPath = Paths.get(directoryPath, file.getOriginalFilename());
-//            Files.write(fileNameAndPath, file.getBytes());
-//
-//            // Lưu đường dẫn của ảnh vào cơ sở dữ liệu
-//            String filename = StringUtils.cleanPath(file.getOriginalFilename());
-//            acc.setAvatar_url(filename);
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//
-//        // Cập nhật thông tin tài khoản
-//        accountService.updateAccount_frUser(acc);
-//        model.addAttribute("acc", acc);
-//        session.set("acc", acc);
-//        System.out.println(acc);
-//        System.out.println("ID: " + acc.getAccount_id());
-//        return "redirect:/home/my-account";
-//    }
-//}
---
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/utils/FileUploadUtil.java
@@ -0,0 +1,26 @@
+package com.poly.website_norulesshop.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileUploadUtil {
+    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFiles) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+        try (InputStream inputStream = multipartFiles.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+}
