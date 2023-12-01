66460f93d2dff8e8d657fce41f7089836c54e4c3
tungit2507 <131683573+tungit2507@users.noreply.github.com>
2023-12-01 18:02:08 +0700

    done forgot

--- Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/service/EmailService.java
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/service/EmailService.java
@@ -1,7 +1,9 @@
 package com.poly.website_norulesshop.service;
 
 import com.poly.website_norulesshop.constant.EmailType;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface EmailService {
     void sendEmail(String to, String code, EmailType emailType);
     void sendEmailContactUs(String to,String email, String content,String subject, String firstName, String lastName);
