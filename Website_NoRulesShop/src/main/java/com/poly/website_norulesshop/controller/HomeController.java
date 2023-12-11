4cc7395a025a351c860321dac44f72531ab87ea0
TrieuBao <baotrieu27122003@gmail.com>
2023-12-11 19:23:28 +0700

    login with facebook

--- Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/HomeController.java
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.poly.website_norulesshop.controller.user;
+package com.poly.website_norulesshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
