9f078687c2dfb613785ee0980888faafc8c05cfd
TrieuBao <baotrieu27122003@gmail.com>
2023-12-19 21:41:38 +0700

    Update APISession.java

--- Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/api/APISession.java
+++ Website_NoRulesShop/src/main/java/com/poly/website_norulesshop/controller/api/APISession.java
@@ -2,19 +2,21 @@ package com.poly.website_norulesshop.controller.api;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/session")
 @RestController
-@RequestMapping("api/session")
-public class APISession {
+public class ApiSession {
     @Autowired
     HttpSession httpSession;
 
-    @GetMapping("/{sessionName}")
-    public Object getSessionAttribute(@PathVariable String sessionName){
-        return httpSession.getAttribute(sessionName);
+    @GetMapping
+    public Object get(@RequestParam String name){
+        return httpSession.getAttribute(name);
+    }
+
+    @PostMapping
+    public void set(@RequestParam String name, @RequestParam Object value){
+        httpSession.setAttribute(name,value);
     }
 }
